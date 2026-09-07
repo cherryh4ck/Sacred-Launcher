0364981a86f80913e74a73c80ae48066a40a6d83
Cherryh4ck <tomas1234567891016@gmail.com>
2026-09-07 17:53:39 -0300

    Finalizar form de selección

---
+++ Form2.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Sacred_Launcher
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.acceptButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.path = new System.Windows.Forms.TextBox();
+            this.pathButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // acceptButton
+            // 
+            this.acceptButton.Location = new System.Drawing.Point(170, 171);
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.Size = new System.Drawing.Size(90, 23);
+            this.acceptButton.TabIndex = 0;
+            this.acceptButton.Text = "Accept";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(12, 25);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(407, 20);
+            this.name.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Path";
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(12, 73);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(326, 20);
+            this.path.TabIndex = 4;
+            // 
+            // pathButton
+            // 
+            this.pathButton.Location = new System.Drawing.Point(344, 73);
+            this.pathButton.Name = "pathButton";
+            this.pathButton.Size = new System.Drawing.Size(75, 22);
+            this.pathButton.TabIndex = 5;
+            this.pathButton.Text = "Select";
+            this.pathButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Description";
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(12, 122);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(404, 20);
+            this.description.TabIndex = 7;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 206);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pathButton);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.acceptButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "Add Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button acceptButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Button pathButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox description;
+    }
+}
