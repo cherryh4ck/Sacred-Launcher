6ebe7e71cb18b4267cccd52caa81407d92b5744a
Cherryh4ck <tomas1234567891016@gmail.com>
2026-09-07 17:45:50 -0300

    Crear interfaz (1)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,72 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gamesList = new System.Windows.Forms.ListBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.gameDescription = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // gamesList
+            // 
+            this.gamesList.FormattingEnabled = true;
+            this.gamesList.Location = new System.Drawing.Point(120, 12);
+            this.gamesList.Name = "gamesList";
+            this.gamesList.Size = new System.Drawing.Size(379, 173);
+            this.gamesList.TabIndex = 0;
+            this.gamesList.SelectedIndexChanged += new System.EventHandler(this.gamesList_SelectedIndexChanged);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(424, 191);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(343, 191);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // gameDescription
+            // 
+            this.gameDescription.Location = new System.Drawing.Point(12, 12);
+            this.gameDescription.Multiline = true;
+            this.gameDescription.Name = "gameDescription";
+            this.gameDescription.Size = new System.Drawing.Size(100, 84);
+            this.gameDescription.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(532, 228);
+            this.Controls.Add(this.gameDescription);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.gamesList);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Sacred Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox gamesList;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TextBox gameDescription;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace Sacred_Launcher
 {
@@ -15,6 +16,46 @@ namespace Sacred_Launcher
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public class Game
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Path { get; set; }
+
+            public override string ToString() => Name;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: load from json file
+            // also show a message if json doesn't exist
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Executables (*.exe)|*.exe";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var game = new Game
+                {
+                    Name = System.IO.Path.GetFileNameWithoutExtension(dialog.FileName),
+                    Path = dialog.FileName,
+                    Description = "aaa"
+                };
+                gamesList.Items.Add(game);
+            }
+        }
+
+        private void gamesList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (gamesList.SelectedItem is Game game)
+            {
+                gameDescription.Text = game.Description;
+            }
         }
     }
 }
