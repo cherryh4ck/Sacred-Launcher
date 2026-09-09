1d62aa72642db06796b637c35edbfd13e889817f
Cherryh4ck <tomas1234567891016@gmail.com>
2026-09-08 21:36:04 -0300

    Añadir aviso en la interfaz del server browser para hacerlo menos confuso

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             this.serverList = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // serverList
@@ -37,16 +38,26 @@
             this.serverList.HideSelection = false;
             this.serverList.Location = new System.Drawing.Point(12, 12);
             this.serverList.Name = "serverList";
-            this.serverList.Size = new System.Drawing.Size(403, 174);
+            this.serverList.Size = new System.Drawing.Size(403, 162);
             this.serverList.TabIndex = 1;
             this.serverList.UseCompatibleStateImageBehavior = false;
             this.serverList.MouseUp += new System.Windows.Forms.MouseEventHandler(this.serverList_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Right click to interact with the list";
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(427, 198);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.serverList);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -57,10 +68,12 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form3_FormClosing);
             this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.ListView serverList;
+        private System.Windows.Forms.Label label1;
     }
 }
